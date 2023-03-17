8750385c20ee4e6d9792115f90d16d44028e85e1
Martsyl <msnonuorah3964@gmail.com>
2023-03-17 15:16:45 -0700

    lower to uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowertoUpper;
+
+	for (lowertoUpper = 'a'; lowertoUpper <= 'z'; char)
+		putchar(lowertoUpper);
+	for (lowertoUpper = 'A'; lowertoUpper <= 'Z'; char)
+		putchar(lowertoUpper);
+	putchar('\n');
+	return (0);
+}

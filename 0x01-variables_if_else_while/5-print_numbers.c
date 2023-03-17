e89b3ea50b85352862ce4417cb03b0d1c28c5d71
Martsyl <msnonuorah3964@gmail.com>
2023-03-17 16:36:17 -0700

    priniting base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numbase;
+
+	for (numbase = 0; numbaese < 10; numbase++)
+		printf("%d", numbase);
+	printf('\n');
+	return (0);
+}

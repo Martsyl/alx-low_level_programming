f9b204ac3f6f0eb5172da9a2643652c959551d13
Martsyl <msnonuorah3964@gmail.com>
2023-03-17 15:20:18 -0700

    lower to upper

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	char lowertoUpper;
 
-	for (lowertoUpper = 'a'; lowertoUpper <= 'z'; char)
+	for (lowertoUpper = 'a'; lowertoUpper <= 'z'; lowertoUpper++)
 		putchar(lowertoUpper);
-	for (lowertoUpper = 'A'; lowertoUpper <= 'Z'; char)
+	for (lowertoUpper = 'A'; lowertoUpper <= 'Z'; lowertoUpper++)
 		putchar(lowertoUpper);
 	putchar('\n');
 	return (0);

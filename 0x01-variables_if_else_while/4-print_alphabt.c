2fff5c677d3d27431c21728c7572c1278129a381
Martsyl <msnonuorah3964@gmail.com>
2023-03-17 16:08:07 -0700

    dont print q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char avoidEQ;
+
+	for (avoidEQ = 'a'; avoidEQ <= 'z' avoidEQ++)
+	{
+		if (avoidEQ != 'e' && avoidEQ != 'q')
+			putchar(avoidEQ);
+	}
+	putchar('\n');
+	return (0);
+}

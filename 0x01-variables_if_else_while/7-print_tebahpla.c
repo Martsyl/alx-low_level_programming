670a2790b6fb52514d9c2245439d489ddaf2de65
Martsyl <msnonuorah3964@gmail.com>
2023-03-17 17:13:54 -0700

    reverse aplha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char aplh;
+
+	for (aplh = 'a'; aplh >= 'z' aplh--)
+		putchar(aplh);
+	putchar('\n');
+	return (0);
+}

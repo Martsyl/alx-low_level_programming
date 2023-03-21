c4e6a65437540ec5a640e11bd092297f82fc0013
Martsyl <msnonuorah3964@gmail.com>
2023-03-21 16:56:30 -0700

    print the last value

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit
+ * @n: the number
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int ls = n % 10;
+
+	if (ld < 0)
+		ld *= -1;
+}

b5613bc6c429503b8aafe7bc302b50ee80fde5b9
Martsyl <msnonuorah3964@gmail.com>
2023-03-21 16:39:40 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computing the abs value
+ * @r: The integer to be computed
+ * Return: The abs value
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (-r);
+}

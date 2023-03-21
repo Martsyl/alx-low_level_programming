86744b7121fbf811aa3ded16072f3b5d3eaa8990
Martsyl <msnonuorah3964@gmail.com>
2023-03-21 12:55:44 -0700

    printing lower alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * print_loweralpha - prints lower alpha
+ * Description: Does my stuff explained.
+ * Return: Always 0 (Success)
+ */
+void print_loweralpha(void)
+{
+	char lw;
+
+	for (lw = 'a'; lw++ <= 'z'; lw++)
+		_putchar(lw);
+	_putchar('\n');
+}

fb661078f4815a2f29fa6504ef4cd518b7806d00
Martsyl <msnonuorah3964@gmail.com>
2023-03-21 17:44:19 -0700

    happy hour

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * jack_bauer-prints
+ *
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int i, j, k, m;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 5; 5++)
+			{
+				for (m = 0; m <= 9; m++)
+				{
+					if (i == 2 && j > 3)
+					{
+						break;
+					}
+				       _putchar(i + '0');
+				       _putchar(j + '0');
+				       _putchar(':');
+				       _putchar(k + '0');
+				       _putchar(m + '0');
+				       _putchar("\n");
+				}
+			}
+		}
+	}
+}

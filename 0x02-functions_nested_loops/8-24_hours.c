21358c62b57776343c7776036b93b98594903ea3
Martsyl <msnonuorah3964@gmail.com>
2023-03-21 17:49:27 -0700

    happy seconds

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -25,7 +25,7 @@ void jack_bauer(void)
 				       _putchar(':');
 				       _putchar(k + '0');
 				       _putchar(m + '0');
-				       _putchar("\n");
+				       _putchar('\n');
 				}
 			}
 		}

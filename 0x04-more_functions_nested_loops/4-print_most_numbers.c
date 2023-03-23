3a76ec6a908b2050f8cb40d3eae4d5cd2b10fbe2
Martsyl <msnonuorah3964@gmail.com>
2023-03-23 14:58:58 -0700

    exculding 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - exculde 2 and 4.
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c =  48; c < 58; c++)
+	{
+		if (c == 50)
+			continue;
+		if (c == 52)
+			continue;
+		_putchar(c);
+		_putchar('\n');
+	}
+}

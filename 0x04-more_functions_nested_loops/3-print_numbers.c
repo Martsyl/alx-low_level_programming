b2218ffc8d730125dec9e0d00dcd033c28cce08b
Martsyl <msnonuorah3964@gmail.com>
2023-03-23 13:53:02 -0700

    print num

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - printing the numbers
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+	{
+		_putchar( a);
+	}
+	_putchar('\n');
+}

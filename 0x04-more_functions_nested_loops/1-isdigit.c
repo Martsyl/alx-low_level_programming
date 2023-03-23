32087a66ba801e318455945c825e4e85d3c15657
Martsyl <msnonuorah3964@gmail.com>
2023-03-23 10:09:33 -0700

    finding numbers

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isdigit - checks for numbers
+ * returns 1 if c is a digit
+ *
+ * @c:integer
+ * Return: 1 if c is a digit or 0 if is an alphabet
+ */
+int _isdigit(int c)
+{
+	if (c == '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}

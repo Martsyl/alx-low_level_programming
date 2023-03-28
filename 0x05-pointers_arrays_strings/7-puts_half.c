a50f707240b1f011de72ec41e145108688480ea9
Martsyl <msnonuorah3964@gmail.com>
2023-03-27 18:04:23 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - check the code
+ * @str:chae
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int getlen = 0;
+
+	while (*str != '\0')
+	{
+		getlen++;
+		str++;
+	}
+	str -= (getlen / 2);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+		_putchar('\n');
+}

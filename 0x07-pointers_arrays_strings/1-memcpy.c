bc996e0efe85fc021f9c18f99fc5dd3773a5fa05
Martsyl <msnonuorah3964@gmail.com>
2023-04-03 07:37:16 -0700

    betty error solved

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,10 +12,10 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int y;
-	
-	for (y = 0; y  < n; y++)
-		{
-			dest[y] = src[y];
-		}
+
+	for (y = 0; y < n; y++)
+	{
+		dest[y] = src[y];
+	}
 	return (dest);
 }

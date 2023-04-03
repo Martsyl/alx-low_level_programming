dad5ec050cfded44125fd60af1798b19156dd551
Martsyl <msnonuorah3964@gmail.com>
2023-04-03 08:10:19 -0700

    strchr function

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - check the codei
+ * @s: character pointer.
+ * @c: character to checked
+ * Return:  str or NULL.
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}

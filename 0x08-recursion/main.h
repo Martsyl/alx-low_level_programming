51d730eb7205647c71e42546403e2f4944593761
Martsyl <msnonuorah3964@gmail.com>
2023-04-05 18:57:14 -0700

    odd~

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,6 +9,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int _sqrt_check(int m, int m2);
+int prime_check(int r, int y);
 int _putchar(char c);
 
 #endif

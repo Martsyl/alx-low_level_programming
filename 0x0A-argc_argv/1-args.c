a63cf0b610166e9f781aeb58de16369ebdb18b8d
Martsyl <msnonuorah3964@gmail.com>
2023-04-09 10:13:32 -0700

    agrc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: int
+ * @argv: char
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	argc = argc - 1;
+
+	printf("%d\n", argc);
+	return (0);
+}

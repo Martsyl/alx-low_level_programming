f36fcedcacb1b2aba6d5157da8eeb3e483769f13
Martsyl <msnonuorah3964@gmail.com>
2023-04-11 10:27:23 -0700

    malloc first

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - prints buffer in hexa
+ * @size: integer
+ * @c:character
+ *
+ * Return: Nothing.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *string;
+	unsigned int i;
+
+	string = malloc(sizeof(char) * size);
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		string[i] = c;
+	}
+	return (string);
+
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+Malloc is mallocing

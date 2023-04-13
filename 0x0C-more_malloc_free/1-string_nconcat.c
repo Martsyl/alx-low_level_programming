21aaadfc3a1e3660f62042188fb14de14219d77a
Martsyl <msnonuorah3964@gmail.com>
2023-04-13 13:19:40 -0700

    cocnat strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - check the code
+ * @s1: character
+ * @s2: character
+ * @n: integer
+ * Return: Always 0.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *concat;
+	unsigned int r1;
+	unsigned int r2;
+	unsigned int i;
+	unsigned int r3;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (r1 = 0; s1[r1] != '\0'; r1++)
+		;
+	for (r2 = 0; s2[r2] != '\0'; r2++)
+		;
+	if (n > r2)
+		n  = r2;
+	r3  = r1 + n;
+	concat = malloc(r3 + 1);
+	if (concat == NULL)
+		return (NULL);
+	for (i = 0; i < r3; i++)
+	{
+		if (i < r1)
+			concat[i] = s1[i];
+		else
+			concat[i] = s2[i - r1];
+	}
+	concat[i] = '\0';
+	return (concat);
+}

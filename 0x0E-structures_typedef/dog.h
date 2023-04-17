550487128f7f0ee5dcb4e10f964d834d98913fb9
Martsyl <msnonuorah3964@gmail.com>
2023-04-17 14:36:49 -0700

    print out

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,4 +21,5 @@ typedef struct dog dog_r;
 void dog_iniit(struct dog *d, char *name, float *age, char *owner);
 dog_r *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif

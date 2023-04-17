8647e6a91da0b8ac2e085b2f113ea9d9d4b1ce44
Martsyl <msnonuorah3964@gmail.com>
2023-04-17 14:24:56 -0700

    done with second

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,4 +20,5 @@ struct dog
 typedef struct dog dog_r;
 void dog_iniit(struct dog *d, char *name, float *age, char *owner);
 dog_r *new_dog(char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif

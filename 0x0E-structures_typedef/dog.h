18dc0d1e29d7c2de44bced4e13a0b786120bf0dc
Martsyl <msnonuorah3964@gmail.com>
2023-04-17 14:07:28 -0700

    dogs

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog  - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/**
+ * dog_r - typedef for struct dog
+ */
+typedef struct dog dog_r;
+void dog_iniit(struct dog *d, char *name, float *age, char *owner);
+dog_r *new_dog(char *name, float age, char *owner);
+#endif

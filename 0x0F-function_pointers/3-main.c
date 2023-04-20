089ec502daa41e2a9c9adc533c3d713f981ba186
Martsyl <msnonuorah3964@gmail.com>
2023-04-20 02:02:04 -0700

    a little error

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -22,7 +22,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	}
 	num1 = atoi(argv[1]);
 	op = argv[2];
-	if (op[1] != '\0')
+	if (op[1] != '\0' || get_op_func(op) == NULL)
 	{
 		printf("Error\n");
 		exit(99);

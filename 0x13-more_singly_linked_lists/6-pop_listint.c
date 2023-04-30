8bed797613ee03741b3fa155df4b5b9446c904e6
Martsyl <msnonuorah3964@gmail.com>
2023-04-30 14:58:10 -0700

    poped out that sht

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * pop_listint - check the code
+ * @head: to be deleted
+ * Return: Always 0.
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *p;
+	int q;
+
+	if (*head == NULL)
+	{
+		return (0);
+	}
+	q = (*head)->n;
+	p = (*head)->next;
+	free(*head);
+	*head = p;
+
+	return (q);
+}

fcbd642768cc3e53a3b082f73e2e18227b799226
Martsyl <msnonuorah3964@gmail.com>
2023-05-04 11:02:31 -0700

    betty error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,15 +15,15 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	for (i = 0; b[i] != '\0'; i++);
-
+	for (i = 0; b[i] != '\0'; i++)
+		;
 	for (i--, get_base = 1; i >= 0; i--, get_base *= 2)
 	{
 		if (b[i] != '0' && b[i] != '1')
 		{
 			return (0);
 		}
-		if(b[i] & 1)
+		if (b[i] & 1)
 		{
 			decimal += get_base;
 		}

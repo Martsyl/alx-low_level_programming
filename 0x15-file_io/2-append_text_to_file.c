8fcd8388b56158bf9ad57bb1608202b9bc987576
Martsyl <msnonuorah3964@gmail.com>
2023-05-08 12:44:25 -0700

    output error

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,12 +19,19 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	fp = open(filename, O_WRONLY | O_APPEND);
-	for (letters = 0; text_content[letters]; letters++)
-		;
-	writefile = write(fp, text_content, letters);
-	if (writefile == -1)
+	if (fp == -1)
 	{
 		return (-1);
+	}
+	if (text_content)
+	{
+		for (letters = 0; text_content[letters]; letters++)
+			;
+		writefile = write(fp, text_content, letters);
+		if (writefile == -1)
+		{
+			return (-1);
+		}
 	}
 	close(fp);
 	return (1);

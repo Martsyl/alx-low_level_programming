0a752066c45c8af7ca0827f8e2b3dccf4a284ad1
Martsyl <msnonuorah3964@gmail.com>
2023-06-07 12:27:44 -0700

    prime nums

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,5 +9,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt_check(int m, int m2);
+int is_prime_number(int n);
+int prime_check(int r, int y);
 
 #endif

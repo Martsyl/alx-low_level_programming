c11f5a70a04aa70355ecfeec04278bd7592e692a
Martsyl <msnonuorah3964@gmail.com>
2023-06-09 12:00:16 -0700

    lib a

--- 0x09-static_libraries/_atoi.c
+++ 0x09-static_libraries/_atoi.c
@@ -11,9 +11,9 @@ int _atoi(char *s)
 	int i = 0;
 	unsigned int result = 0;
 
-	while ((!s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
 	{
-		if (s[i] == '-')
+		if (s[i] == '_')
 			sgn *= -1;
 		i++;
 	}

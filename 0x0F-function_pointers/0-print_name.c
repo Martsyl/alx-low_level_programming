48fbb109f4b6cb97dee11aab3086a3dc1c332d67
Martsyl <msnonuorah3964@gmail.com>
2023-06-21 07:58:23 -0700

    created func.h

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * print_name - prints a name as is
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -9,3 +9,4 @@ int int_index(int *array, int size, int (*cmp)(int));
 int _putchar(char c);
 int (*get_op_func(char *s))(int, int);
 #endif
+
